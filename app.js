816adb0965c5c2095d19175eb0c04d6fc76ff6ce
YuHsuanEmilyChen <emily.yuhsuan@gmail.com>
2021-12-07 22:13:09 +0800

    feat:Modify card and search routes for using Mongoose

--- app.js
+++ app.js
@@ -27,9 +27,9 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 // Set routes
-// CRUD - Read
+// CRUD - Read index
 app.get('/', (req, res) => {
-  Restaurant.find()
+  return Restaurant.find()
     .lean()
     .then(diners => res.render('index', { diners }))
     .catch(error => console.log('error'))
@@ -38,20 +38,40 @@ app.get('/', (req, res) => {
 //   res.render('index', { diners: restaurantList.results })
 // })
 
-
+// CRUD - Read card
 app.get('/restaurants/:diner_id', (req, res) => {
-  const diner = restaurantList.results.find(diner => diner.id.toString() === req.params.diner_id)
-  res.render('card', { dinerOne: diner })
+  return Restaurant.findById(req.params.diner_id)
+    .lean()
+    .then(diner => res.render('card', { dinerOne: diner }))
+    .catch(error => console.log('error'))
 })
 
+// app.get('/restaurants/:diner_id', (req, res) => {
+//   const diner = restaurantList.results.find(diner => diner.id.toString() === req.params.diner_id)
+//   res.render('card', { dinerOne: diner })
+// })
+
+
+// CRUD - Read search result
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword.trim()
-  const diners = restaurantList.results.filter(diner => {
-    return diner.name.toLowerCase().includes(keyword.toLowerCase()) || diner.category.includes(keyword)
-  })
-  // object literal extension { diners: diners, keyword: keyword}
-  res.render('index', { diners, keyword })
+  return Restaurant.find()
+    .lean()
+    .then(diners => {
+      const filtered_diner = diners.filter(diner => {
+        diner.name.toLowerCase().includes(keyword.toLowerCase()) || diner.category.includes(keyword)
+      })
+      res.render('index', { diners: filtered_diner, keyword })
+    })
+    .catch(error => console.log('error'))
+
+  // const diners = restaurantList.results.filter(diner => {
+  //   return diner.name.toLowerCase().includes(keyword.toLowerCase()) || diner.category.includes(keyword)
+  // })
+  // // object literal extension { diners: diners, keyword: keyword}
+  // res.render('index', { diners, keyword })
 })
+
 
 // Start and Listen on the Express server
 app.listen(port, () => {
