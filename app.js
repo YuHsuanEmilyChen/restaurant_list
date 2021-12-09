30b14e8759d7ac5382bd1f1539d548471e9ae484
YuHsuanEmilyChen <emily.yuhsuan@gmail.com>
2021-12-09 19:46:43 +0800

    feat:fix bugs

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-// const restaurantList = require('./restaurant.json')
 // const Swal = require('sweetalert2')
 
-// 載入model
+// Load model
 const Restaurant = require('./models/restaurant')
 
 // Connect MongoDB
@@ -21,28 +20,26 @@ db.once('open', () => {
 })
 
 // Create a custom helper
-const hbs = exphbs.create({
-  defaultLayout: 'main',
-  helpers: {
-    searchAlert: function (keyword) {
-      return `<script> Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: '${keyword}',
-        confirmButtonText:'OK'
-      })</script>`
-    },
-    sayHi: function (keyword) {
-      return "<h1>" + keyword + "</h1>"
-    }
-  }
-})
+// const hbs = exphbs.create({
+//   defaultLayout: 'main',
+//   helpers: {
+//     searchAlert: function (keyword) {
+//       return `<script> Swal.fire({
+//         icon: 'error',
+//         title: 'Oops...',
+//         text: '${keyword}',
+//         confirmButtonText:'OK'
+//       })</script>`
+//     },
+//     sayHi: function (keyword) {
+//       return "<h1>" + keyword + "</h1>"
+//     }
+//   }
+// })
 
 // Set template engine
-app.engine('handlebars', hbs.engine)
-// app.engine('handlebars', exphbs({
-//   defaultLayout: 'main',
-// }))
+// app.engine('handlebars', hbs.engine)
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 // Set static files
@@ -54,25 +51,16 @@ app.get('/', (req, res) => {
   return Restaurant.find()
     .lean()
     .then(diners => res.render('index', { diners }))
-    .catch(error => console.log('error'))
+    .catch(error => console.log('index error'))
 })
-// app.get('/', (req, res) => {
-//   res.render('index', { diners: restaurantList.results })
-// })
 
-// CRUD - Read card
-app.get('/restaurants/:diner_id', (req, res) => {
-  return Restaurant.findById(req.params.diner_id)
+// CRUD - Read detail
+app.get('/restaurants/:id', (req, res) => {
+  return Restaurant.findById(req.params.id)
     .lean()
-    .then(diner => res.render('card', { dinerOne: diner }))
-    .catch(error => console.log('error'))
+    .then((diner) => { res.render('card', { dinerOne: diner }) })
+    .catch(error => console.log('card error'));
 })
-
-// app.get('/restaurants/:diner_id', (req, res) => {
-//   const diner = restaurantList.results.find(diner => diner.id.toString() === req.params.diner_id)
-//   res.render('card', { dinerOne: diner })
-// })
-
 
 // CRUD - Read search result
 app.get('/search', (req, res) => {
@@ -86,12 +74,6 @@ app.get('/search', (req, res) => {
       res.render('index', { diners: filtered_diner, keyword })
     })
     .catch(error => console.log('error'))
-
-  // const diners = restaurantList.results.filter(diner => {
-  //   return diner.name.toLowerCase().includes(keyword.toLowerCase()) || diner.category.includes(keyword)
-  // })
-  // // object literal extension { diners: diners, keyword: keyword}
-  // res.render('index', { diners, keyword })
 })
 
 
